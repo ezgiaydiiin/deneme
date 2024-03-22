94890a2780f73726bd6fb8ab6ac5fe764d3e49a1
Ster <ebrar.sipal@hotmail.com>
2024-03-22 13:10:09 +0300

    13.09

--- yazmuhflutter/lib/main.dart
+++ yazmuhflutter/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       darkTheme: ThemeData.dark(),
-      title: 'ebrar begum sipal',
+      title: 'ebrar begum ',
       home: const ButtonLearn(),
     );
   }
